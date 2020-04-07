e444b3e75b21e5e3e1ec4d9bb85421f19cfd8ba4
Zhenyu Lei <52066383+lei37927@users.noreply.github.com>
2020-04-07 12:20:51 -0700

    With overlay

--- snaw-frontend/src/components/AnalyzeButton.js
+++ snaw-frontend/src/components/AnalyzeButton.js
@@ -3,19 +3,17 @@ import Button from '@material-ui/core/Button';
 import { Link } from 'react-router-dom';
 import { withStyles} from "@material-ui/core/styles";
 import { withRouter } from 'react-router-dom';
-import AssessmentIcon from '@material-ui/icons/Assessment';
-import Tooltip from "recharts/lib/component/Tooltip";
 
 const useStyles = theme => ({
     button: {
         color: 'white',
         fontSize: '1em',
-        backgroundColor: '#AA4C39',
+        backgroundColor: '#3f5a14',
         margin: theme.spacing(1),
         paddingLeft: theme.spacing(4),
         paddingRight: theme.spacing(4),
         '&:hover': {
-            background: '#550F00',
+            background: '#2e420e',
         },
     },
 });
@@ -33,7 +31,8 @@ class AnalyzeButton extends React.Component {
 
     showLoadingImage = event => {
         event.preventDefault();
-        document.getElementById("ldimg").style.display='block';
+        document.getElementById("overlay").style.display='block';
+        document.getElementById("anlzbtn").innerHTML='Analyzing Audio...';
         setTimeout( event => { this.props.history.push('/results'); }, 100);
     };
 
@@ -44,17 +43,18 @@ class AnalyzeButton extends React.Component {
                     <label htmlFor="outlined-button-file">
                         {this.props.bool ? (
                             <Link to={'/results'} onClick={this.showLoadingImage} style={{ textDecoration: 'none' }}>
-                                    <Button disabled={false}
-                                            variant="contained"
-                                            className={classes.button}>
-                                        Analyze Audio File(s)
-                                    </Button>
+                                <Button disabled={false} 
+                                        variant="contained"
+                                        className={classes.button}
+                                        id="anlzbtn">
+                                    Analyze Audio
+                                </Button>
                             </Link>
                             ) : (
-                                <Button disabled={true}
-                                    variant="contained"
-                                    className={classes.button}>
-                                    Please upload file(s)
+                                <Button disabled={true} 
+                                        variant="contained" 
+                                        className={classes.button}>
+                                    Analyze Audio
                                 </Button>
                         )}
                     </label>
