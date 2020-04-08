2003a9d79af05dde098f8ce80b959bfd0d60ec9a
Mike Ewers <creativedevvers@gmail.com>
2020-04-08 12:50:49 -0700

    Removed Spectral indices due to errors

--- snaw-backend/acousticIndices.py
+++ snaw-backend/acousticIndices.py
@@ -5,7 +5,7 @@ import peakutils
 import SimpleITK as sitk
 from scipy.stats import itemfreq
 import os
-
+import traceback
 DEBUG_FLAG = True
 
 class AcousticIndices(object):
@@ -620,7 +620,7 @@ class AcousticIndices(object):
         feature_vector.append(self.get_mid_band_activity())
         feature_vector.append(self.get_spectral_maxima_entropy())
         feature_vector.extend(self.get_spectral_average_variance_entropy())
-        feature_vector.extend(self.get_spectral_diversity_persistance())
+        #feature_vector.extend(self.get_spectral_diversity_persistance())
 
         return feature_vector
 
@@ -644,8 +644,8 @@ class AcousticIndices(object):
         feature_headers.append("Entropy Of Spectral Maxima")
         feature_headers.append("Entropy Of Spectral Average")
         feature_headers.append("Entropy Of Spectral Variance")
-        feature_headers.append("Spectral Diversity")
-        feature_headers.append("Spectral Persistence")
+        #feature_headers.append("Spectral Diversity")
+        #feature_headers.append("Spectral Persistence")
 
         return feature_headers
 
@@ -674,40 +674,46 @@ def getAcousticIndices(peronsalID):
 
     # Create file counter
     fileCount = 0
-
     if( DEBUG_FLAG ):
         print("[WORKING] Attempting to run acoustic indices calculator - acousticIndices.py")
     # loop through the files in the directory
-    for file in os.listdir('instance/upload/user'+peronsalID):
-        # correct the file path with the prefixed upload folder
-        filePath = 'instance/upload/user'+peronsalID+"/" + file
-        data,fs  =  librosa.load(filePath,sr=None,offset=0,duration=60)
+    try:
+        for file in os.listdir('instance/upload/user'+peronsalID):
+            # correct the file path with the prefixed upload folder
+            filePath = 'instance/upload/user'+peronsalID+"/" + file
+            data,fs  =  librosa.load(filePath,sr=None,offset=0,duration=60)
 
-        # mono channel
-        data = AudioProcessing.convert_to_mono(data)
+            # mono channel
+            data = AudioProcessing.convert_to_mono(data)
 
-        # changing sampling rate
-        new_fs = 17640
-        data_chunk = AudioProcessing.resample(data,fs,new_fs)
+            # changing sampling rate
+            new_fs = 17640
+            data_chunk = AudioProcessing.resample(data,fs,new_fs)
 
-        # extracting indices
-        acousticIndices = AcousticIndices(data_chunk,new_fs)
-        acoustic_indices = acousticIndices.get_acoustic_indices()
-        acoustic_headers = acousticIndices.get_acoustic_indices_headers()
 
-        # singleResultArray is used to store the results of one file (List of dictionaries)
-        singleResultArray = []
+            # extracting indices
+            acousticIndices = AcousticIndices(data_chunk,new_fs)
+            acoustic_indices = acousticIndices.get_acoustic_indices()
+            print("Hi: ", len(acoustic_indices))
+            acoustic_headers = acousticIndices.get_acoustic_indices_headers()
+            print("Headers", len(acoustic_headers))
+            # singleResultArray is used to store the results of one file (List of dictionaries)
+            singleResultArray = []
 
-        # Traverse the acoustic tags
-        for i in range(len(acoustic_headers)):
-            # per indices in the length of the acoustic tags,
-            # append dictionary items.
-            singleResultArray.append({"index": acoustic_headers[i], "value" : acoustic_indices[i]})
-        # append result dictionary to the final results array
-        if( DEBUG_FLAG ):
-            print("[WORKING] Calculated " + acoustic_headers[i] + " - acousticIndices.py")
-        fileDictionary[fileCount] = singleResultArray
-        fileCount += 1
+            # Traverse the acoustic tags
+            for i in range(len(acoustic_headers)):
+                # per indices in the length of the acoustic tags,
+                # append dictionary items.
+                singleResultArray.append({"index": acoustic_headers[i], "value" : acoustic_indices[i]})
+
+            # append result dictionary to the final results array
+                if( DEBUG_FLAG ):
+                    print("[WORKING] Calculated " + acoustic_headers[i] + " - acousticIndices.py")
+            fileDictionary[fileCount] = singleResultArray
+            fileCount += 1
+    except Exception as e:
+        track = traceback.format_exc()
+        print(track)
 
     if( DEBUG_FLAG ):
         print("[SUCCESS] Calculated acoustic indices - acousticIndices.py")
