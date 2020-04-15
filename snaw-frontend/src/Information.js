bd1c57290120e6cace020a3276bb4892eeabecfe
Josh <heyjoshkruse@gmail.com>
2020-04-15 14:45:55 -0700

    updates to the information page were made

--- snaw-frontend/src/Information.js
+++ snaw-frontend/src/Information.js
@@ -5,6 +5,9 @@ import Divider from "@material-ui/core/Divider";
 import Grid from "@material-ui/core/Grid";
 import List from "@material-ui/core/List";
 import ListItem from "@material-ui/core/ListItem";
+import Box from "@material-ui/core/Box";
+import nn_arch from './img/nn.png'
+import CardMedia from "@material-ui/core/CardMedia";
 
 const customtheme = createMuiTheme({
     palette : {
@@ -24,59 +27,63 @@ function Information() {
                 <MuiThemeProvider theme={customtheme}>
                     <Grid container spacing={5} direction='column'>
                         <Paper elevation={3}>
-                            <Grid item>
-                                <Typography variant='h3' style={{color:customtheme.palette.primary.dark}}>
-                                    Why?
-                                </Typography>
-                                <Divider middle></Divider>
-                                <Typography>Various environmental changes affect a range of species around the world
-                                    and as more species are being affected, proper management and observation are
-                                    required to understand their response. Traditional field methods require trained
-                                    observers to determine species presence/absence and are thus expensive and
-                                    challenging to employ at large scales. Using sound to monitor biodiversity across
-                                    landscapes is a fairly recent development.</Typography>
-                                <Divider/>
-                            </Grid>
-                            <Grid item>
-                                <Typography variant='h3' style={{color:customtheme.palette.primary.dark}}>
-                                    What?
-                                </Typography>
-                                <Divider middle></Divider>
-                                <Typography>Using a Convolutional Neural Network, the Soundscape Noise Analysis Workbench
-                                allows biodiversity researchers and volunteers to easily, quickly, and automatically
-                                identify individual sound components from their uploaded soundscape files.</Typography>
-                                <Typography>SNAW identifies sounds in three categories</Typography>
-                                <List>
-                                    <ListItem>
-                                        <ListItemText
-                                            primary='Anthrophony'
-                                            secondary='Sounds made by humans'/>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemText
-                                            primary='Biophony'
-                                            secondary='Sounds made by biological life'/>
-                                    </ListItem>
-                                    <ListItem>
-                                        <ListItemText
-                                            primary='Geophony'
-                                            secondary='Sounds made by nature'/>
-                                    </ListItem>
-                                </List>
-                                <Divider/>
-                            </Grid>
-                            <Grid item>
-                                <Typography>When</Typography>
-                                <Divider/>
-                            </Grid>
-                            <Grid item>
-                                <Typography>Where</Typography>
-                                <Divider/>
-                            </Grid>
-                            <Grid item>
-                                <Typography>Why</Typography>
-                                <Divider/>
-                            </Grid>
+                            <Box p={5}>
+                                <Grid item>
+                                    <Typography variant='h3' style={{color:customtheme.palette.primary.dark}}>
+                                        Motivations
+                                    </Typography>
+                                    <Divider middle></Divider>
+                                    <Typography>Various environmental changes affect a range of species around the world
+                                        and as more species are being affected, proper management and observation are
+                                        required to understand their response. Traditional field methods require trained
+                                        observers to determine species presence/absence and are thus expensive and
+                                        challenging to employ at large scales. Using sound to monitor biodiversity across
+                                        landscapes is a fairly recent development.</Typography>
+                                    <Divider/>
+                                </Grid>
+                                <Grid item>
+                                    <Typography variant='h3' style={{color:customtheme.palette.primary.dark}}>
+                                        What is SNAW
+                                    </Typography>
+                                    <Divider middle></Divider>
+                                    <Typography>Using a Convolutional Neural Network, the Soundscape Noise Analysis Workbench
+                                    allows biodiversity researchers and volunteers to easily, quickly, and automatically
+                                    identify individual sound components from their uploaded soundscape files.</Typography>
+                                    <Typography>SNAW identifies sounds in three categories</Typography>
+                                    <List>
+                                        <ListItem>
+                                            <ListItemText
+                                                primary='Anthrophony'
+                                                secondary='Sounds made by humans'/>
+                                        </ListItem>
+                                        <ListItem>
+                                            <ListItemText
+                                                primary='Biophony'
+                                                secondary='Sounds made by biological life'/>
+                                        </ListItem>
+                                        <ListItem>
+                                            <ListItemText
+                                                primary='Geophony'
+                                                secondary='Sounds made by nature'/>
+                                        </ListItem>
+                                    </List>
+                                    <Divider/>
+                                </Grid>
+                                <Grid item>
+                                    <Typography>Architecture</Typography>
+                                    <CardMedia id="nn_arch" component='img' image={nn_arch}
+                                               className="classes.media"/>
+                                    <Divider/>
+                                </Grid>
+                                <Grid item>
+                                    <Typography>Where</Typography>
+                                    <Divider/>
+                                </Grid>
+                                <Grid item>
+                                    <Typography>Why</Typography>
+                                    <Divider/>
+                                </Grid>
+                            </Box>
                         </Paper>
                     </Grid>
                 </MuiThemeProvider>
