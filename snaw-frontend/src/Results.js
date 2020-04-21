6f790545e5c1c21adc82f20c9d3021ec381baa21
Mike Ewers <creativedevvers@gmail.com>
2020-04-21 03:39:51 -0700

    Results.js error message change

--- snaw-frontend/src/Results.js
+++ snaw-frontend/src/Results.js
@@ -275,8 +275,10 @@ function Results() {
                                             <Typography>
                                                 <h3>It seems something went wrong with this file! Here's what we ran into:</h3>
                                                 <ListItemText>
-                                                {listOfProblems.map(item => <p><h2>{item}</h2></p>)}
+                                                {listOfProblems.map(item => <p><h4>{item}</h4></p>)}
                                                 </ListItemText>
+                                                <br/>
+                                                <h3>If this problem persists, please contact [support@example.com]</h3>
                                             </Typography>
                                         </Paper>
                                     </Container>
